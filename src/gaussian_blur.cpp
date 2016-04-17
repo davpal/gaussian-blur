c36c5b2cfb9f1e9831f08a3ff2fc06b64cc5c525
davpal <paluchdaw@gmail.com>
2016-04-17 12:42:42 +0200

    Link executable with cimg libraries

--- src/gaussian_blur.cpp
+++ src/gaussian_blur.cpp
@@ -1,2 +1,19 @@
-int main() {
+#include <iostream>
+#include <CImg.h>
+
+using namespace cimg_library;
+
+void help();
+
+int main(int argc, char * argv[]) {
+    if(argc < 2) {
+        help();
+        exit(0);
+    }
+
+    CImg<unsigned char> image(argv[1]);
+}
+
+void help() {
+    std::cout << "usage: gaussian_blur <image>\n";
 }
